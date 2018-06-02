0b383bc1ddf1cf72c56cd89427b85086f46a4768
root <fields.matt11@gmail.com>
2018-06-02 10:01:20 -0400

    LLH 6/2

--- server/index.js
+++ server/index.js
@@ -6,8 +6,10 @@ const app = module.exports = express();
 // app.use( express.static(__dirname + '../build'));
 // app.use(express.static(__dirname + '/default.htm'));
 // app.use(express.static(path.join(__dirname , '/build')));
-app.use( express.static(__dirname+'/../build'));
-
+app.use( express.static(`${__dirname}/../build`));
+// app.get('/', (req, res)=>{
+//     res.sendFile(path.join(__dirname, '../build/index.html'));
+// });
 
 
 const port = 8080;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 import tabtwo from './tabtwo';
 import tabtwob from './tabtwob';
 import tabthree from './tabthree';
